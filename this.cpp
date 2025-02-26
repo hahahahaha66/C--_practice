1380e3bcc2791f121820aaa4dd70082dda899f7a
hahaha <a310608318@gmail.com>
2025-02-26 22:50:41 +0800

    2.26

--- this.cpp
+++ this.cpp
@@ -1,6 +1,6 @@
 //在类内，对象成员和类内代码是分开的 
 //c++通过this指针来区分不同的对象成员的数据
-//this指针永远指向当前的对象
+//this指针永远指向当前的对象 也就是对象指针
 #include <iostream>
 using namespace std;
 class maker{
---
+++ 运算符重载.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//using namespace std;
+//定义是赋予运算符另一种含义
+//重载函数本质是一种编译器调用的函数调用
+class Maker{
+    public:
+    Maker(){
+        a=10;
+        b=20;
+    }
+    Maker (int a,int b){
+        this->a=a;
+        this->b=b;
+    }
+    //成员重载函数，只需要一个右边的参数，左边是this 
+    Maker operator+(Maker &m){
+        Maker result(this->a+m.a,this->b+m.b);
+        return result;
+    }
+    public:
+    int a;
+    int b;
+};
+//全局重载函数
+Maker operator+(Maker&m,Maker&n){
+    Maker result(m.a+n.a,m.b+m.b);
+    return result;
+}
